74b4673bf97e35617bfb4621d90a2c129d284e9a
tsmaz <tsmazich@cpp.edu>
2025-03-28 23:47:46 -0700

    Added test program in main

--- magicsquare.c
+++ magicsquare.c
@@ -55,28 +55,84 @@ int isLoShu(int square[3][3]) {
     return 1;
 }
 
-void printSquare(int square[3][3]) {
+void printSquare(int square[3][3]) 
+{
     int i, j;
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+
+    for (i = 0; i < 3; i++) 
+    {
+        for (j = 0; j < 3; j++) 
+        {
             printf("%d ", square[i][j]);
         }
+
         printf("\n");
     }
 }
 
-void fillRandomSquare(int square[3][3]) {
+void fillRandomSquare(int square[3][3]) 
+{
     int used[10] = {0}; // Track numbers 1..9
     int i, j;
 
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    for (i = 0; i < 3; i++) 
+    {
+        for (j = 0; j < 3; j++) 
+        {
             int r;
-            do {
+            do 
+            {
                 r = rand() % 9 + 1; // Random number between 1 and 9
             } while (used[r] == 1); // If already used, pick again
+            
             used[r] = 1;
             square[i][j] = r;
         }
     }
+}
+
+int main() 
+{
+    srand(585584);
+
+    // Valid square
+    int square1[3][3] = {
+        {4, 9, 2},
+        {3, 5, 7},
+        {8, 1, 6}
+    };
+
+    // Invalid square
+    int square2[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    printSquare(square1);
+    if (isLoShu(square1)) {
+        printf("Square 1 is a Lo Shu Square\n\n");
+    } else {
+        printf("Square 1 is not a Lo Shu Square\n\n");
+    }
+
+    printSquare(square2);
+    if (isLoShu(square2)) {
+        printf("Square 2 is a Lo Shu Square\n\n");
+    } else {
+        printf("Square 2 is not a Lo Shu Square\n\n");
+    }
+
+    int randomSquare[3][3];
+    int numberOfGenerations = 0;
+
+    do {
+        fillRandomSquare(randomSquare);
+        numberOfGenerations++;
+    } while (!isLoShu(randomSquare));
+
+    printf("Squares generated: %d\n", numberOfGenerations);
+    printSquare(randomSquare);
+
+    return 0;
 }
